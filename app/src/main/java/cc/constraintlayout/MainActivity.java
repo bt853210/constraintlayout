bc3022c1129caa029ae28516b6686824cd947883
bt853210 <bt853210@gmail.com>
2019-03-14 13:07:06 +0800

    放大

--- app/src/main/java/cc/constraintlayout/MainActivity.java
+++ app/src/main/java/cc/constraintlayout/MainActivity.java
@@ -2,6 +2,7 @@ package cc.constraintlayout;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -9,5 +10,13 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    int size = 30;//sp
+
+    public void bigger(View v){
+        textView hello;
+        hello = findViewByld(R.id.textView);
+        hello.setTextSize(++size) ;
     }
 }
